edc2ac17cb1f1ca14c8804c9a69f650e2a5a7a77
nino kristesiashvili <kristesiashvilinino@gmail.com>
2025-06-06 08:40:35 +0400

    added store basket

--- src/Services/Basket.API/Features/StoreBasket/StoreBasketEndpoint.cs
+++ src/Services/Basket.API/Features/StoreBasket/StoreBasketEndpoint.cs
@@ -1,6 +1,18 @@
+using Basket.API.Models;
+using Carter;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Basket.API.Features.StoreBasket;
 
-public class StoreBasketEndpoint
+public class StoreBasketEndpoint: ICarterModule
 {
-    
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/bakset", async ([FromBody] ShoppingCart cart, ISender sender) =>
+        {
+            await sender.Send(new StoreBasketCommand(cart));
+            return Results.Ok(cart);
+        });
+    }
 }
--- src/Services/Basket.API/Features/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket.API/Features/StoreBasket/StoreBasketHandler.cs
@@ -1,6 +1,21 @@
+using Basket.API.Models;
+using BuildingBlocks.CQRS;
+using Marten;
+
 namespace Basket.API.Features.StoreBasket;
 
-public class StoreBasketHandler
+public record StoreBasketResult(ShoppingCart cart);
+
+public record StoreBasketCommand(ShoppingCart cart) : ICommand<StoreBasketResult>;
+
+public class StoreBasketHandler(IDocumentSession session): ICommandHandler<StoreBasketCommand, StoreBasketResult>
 {
-    
+    public Task<StoreBasketResult> Handle(StoreBasketCommand request, CancellationToken cancellationToken)
+    {
+        var cart = new ShoppingCart(request.cart.UserName);
+        cart.Items.AddRange(request.cart.Items);
+        session.Store(cart);
+        
+        return Task.FromResult(new StoreBasketResult(cart));
+    }
 }
