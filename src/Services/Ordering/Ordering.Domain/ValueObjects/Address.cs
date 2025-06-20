fc0dc594a4a2d90d2e265c0a86e586134d91c2af
nino kristesiashvili <kristesiashvilinino@gmail.com>
2025-06-20 08:53:45 +0400

    added validations for domain value objects

--- src/Services/Ordering/Ordering.Domain/ValueObjects/Address.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/Address.cs
@@ -5,10 +5,35 @@ public record Address
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; }= string.Empty;
     public string Email { get; set; }= string.Empty;
-    public string AddressLine1 { get; set; }= string.Empty;
-    public string AddressLine2 { get; set; }= string.Empty;
+    public string AddressLine { get; set; }= string.Empty;
     public string City { get; set; }= string.Empty;
     public string State { get; set; }= string.Empty;
     public string Country { get; set; }= string.Empty;
     public string ZipCode { get; set; }= string.Empty;
+
+    private Address(string firstName, string lastName, string email, string addressLine,
+        string city, string state, string country, string zipcode)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        AddressLine = addressLine;
+        City = city;
+        State = state;
+        Country = country;
+        ZipCode = zipcode;
+    }
+
+    public static Address Of(string firstName, string lastName, string email, string addressLine,
+        string city, string state, string country, string zipcode)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(email);
+        ArgumentException.ThrowIfNullOrEmpty(addressLine);
+        ArgumentException.ThrowIfNullOrEmpty(city);
+        ArgumentException.ThrowIfNullOrEmpty(state);
+        ArgumentException.ThrowIfNullOrEmpty(country);
+        ArgumentException.ThrowIfNullOrEmpty(zipcode);
+        return new Address(firstName, lastName, email, addressLine, city, state, country, zipcode);
+    }
+    
 }
--- src/Services/Ordering/Ordering.Domain/ValueObjects/Payment.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/Payment.cs
@@ -1,10 +1,59 @@
+using System.Text.RegularExpressions;
+
 namespace Ordering.Domain.ValueObjects;
 
 public record Payment
 {
-    public string CardNumber { get; set; } = string.Empty;
-    public string CardHolderName { get; set; } = string.Empty;
-    public string ExpiryDate { get; set; } = string.Empty;
-    public string CVV { get; set; } = string.Empty;
-    public string PaymentMethod { get; set; } = string.Empty;
+    public string CardNumber { get;  } = string.Empty;
+    public string CardHolderName { get; } = string.Empty;
+    public string ExpiryDate { get;  } = string.Empty;
+    public string CVV { get;  } = string.Empty;
+    public string PaymentMethod { get;  } = string.Empty;
+
+    private Payment(string cardNumber, string cardHolderName, string expiryDate, string cvv, string paymentMethod)
+    {
+        CardNumber = cardNumber;
+        CardHolderName = cardHolderName;
+        ExpiryDate = expiryDate;
+        CVV = cvv;
+        PaymentMethod = paymentMethod;
+    }
+
+    public static Payment Of(string cardNumber, string cardHolderName, string expiryDate, string cvv,
+        string paymentMethod)
+    {
+        // Supports both MM/YY and MM/YYYY
+        var regex = new Regex(@"^(0[1-9]|1[0-2])\/(([0-9]{2})|([0-9]{4}))$");
+        ArgumentException.ThrowIfNullOrEmpty(cardNumber);
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(cardNumber.Length, 16);
+        ArgumentOutOfRangeException.ThrowIfLessThan(cardNumber.Length, 16);
+        ArgumentException.ThrowIfNullOrEmpty(cardNumber);
+        ArgumentException.ThrowIfNullOrEmpty(expiryDate);
+        ArgumentException.ThrowIfNullOrEmpty(cvv);
+        ArgumentOutOfRangeException.ThrowIfLessThan(cvv.Length, 3);
+        if(!regex.IsMatch(expiryDate))
+            throw new DomainException("Invalid expiry Date format, it must be MM/YY or MM/YYYY");
+        CheckCardExpiration(expiryDate);
+        
+        ArgumentException.ThrowIfNullOrEmpty(paymentMethod);
+        ArgumentException.ThrowIfNullOrEmpty(cardHolderName);
+
+        return new Payment( cardNumber, cardHolderName, expiryDate, cvv, paymentMethod);
+    }
+
+    private static bool CheckCardExpiration(string expirydate)
+    {
+        var parts = expirydate.Split('/');
+        int month = int.Parse(parts[0]);
+        int year = int.Parse(parts[1]);
+        // Create a date for the last day of the expiry month
+        var expiryDate = new DateTime(year, month, 1).AddMonths(1).AddDays(-1);
+        if (expiryDate < DateTime.Today)
+        {
+            throw new DomainException("Card seems to be expired");
+        }
+
+        return false;
+
+    }
 }
